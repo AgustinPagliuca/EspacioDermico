777ef7c359d5607703cc743f82abe9f5e328fd39
Agustin Pagliuca <agustinpagliuca1@gmail.com>
2025-12-29 15:56:11 -0300

    Fix vite config base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: 'https://espaciodermico.com.ar',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
