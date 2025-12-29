1b6a67117f0f4c5f81e63c9e9c5776e43f151d1d
Agustin Pagliuca <agustinpagliuca1@gmail.com>
2025-12-29 15:48:05 -0300

    vite config change

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
+  base: 'https://espaciodermico.com.ar',
   plugins: [react()],
   resolve: {
     alias: {
